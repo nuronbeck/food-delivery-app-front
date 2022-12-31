cff4b066e3776c9c73b8b1244f4963079b3b2368
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-01-01 04:39:46 +0500

    Some fixes

--- src/api/index.js
+++ src/api/index.js
@@ -4,4 +4,10 @@ const client = axios.create({
   baseURL: 'https://food-delivery-app-api.netlify.app'
 });
 
+client.interceptors.request.use(function (config) {
+  config.headers['Content-Type'] = 'application/json';
+
+  return config;
+})
+
 export default client
